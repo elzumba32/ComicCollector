d2400d119c9aa7a75b1f4841b079a82d919ce92e
elzumba32 <elzumba32@gmail.com>
2026-09-13 20:29:38 -0300

    pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
 import { BottomNav } from "@/components/layout/BottomNav";
+import { ServiceWorkerRegistration } from "@/components/layout/ServiceWorkerRegistration";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-gray-50 pb-16 md:pb-0">
+        <ServiceWorkerRegistration />
         <Navbar />
         <main className="flex-1">{children}</main>
         <BottomNav />
